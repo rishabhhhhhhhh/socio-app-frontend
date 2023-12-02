ad01aae4214258f140e647cae7b7d9ce9b511334
rishabhhhhhhhh <p.rishabh.arora@gmail.com>
2023-12-02 18:57:10 +0530

    updating server url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 // export const SERVER_URL = 'https://socio-app-backend.vercel.app';
-export const SERVER_URL = 'http://localhost:3001';
+// export const SERVER_URL = 'http://localhost:3001';
+export const SERVER_URL = 'https://socio-app-backend.vercel.app';
 
 
 export const getBase64 = async (file, callback) => {
